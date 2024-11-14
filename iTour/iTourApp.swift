f50d5472d0431cba8ef135fc2b6eb6a5e38040e2
Nick Tkachenko <piranya87@gmail.com>
2024-11-14 18:31:47 +0000

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Nick Tkachenko on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
